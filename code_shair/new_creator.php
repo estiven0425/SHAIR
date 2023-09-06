8fb76ba6b5bb5aac6145ac3b1b505a430c95976d
Estiven Montoya Torres <114525528+estiven0425@users.noreply.github.com>
2023-09-06 12:12:50 -0500

    Update new_creator.php

--- code_shair/new_creator.php
+++ code_shair/new_creator.php
@@ -23,7 +23,7 @@
         #new_creator { /*Estilo al bloque de subida*/
             align-items: center; /*Alineamos en el centro vertical*/
             background: radial-gradient(circle, rgb(190, 200, 244) 0%, rgb(135, 155, 224) 100%); /*Color de fondo*/
-            border:  groove 2px rgb(135, 155, 224); /*Estilo, grosor y color de borde*/
+            border:  solid 2px rgb(135, 155, 224); /*Estilo, grosor y color de borde*/
             border-radius: 25px; /*Radio de las esquinas*/
             box-shadow: 0 0 20px rgba(30, 55, 214); /*Sombra*/
             display: flex; /*Display flexible*/
@@ -69,83 +69,92 @@
             width: 100%; /*Tamaño*/
         }
 
+        #new_creator input[type="password"] { /*Estilo para los input tipo email*/
+            border: 1px groove rgb(135, 155, 224); /*Borde*/
+            border-radius: 10px; /*Radio de las esquinas*/
+            box-shadow: 0 0 20px rgba(30, 55, 114, 0.550); /*Sombra*/
+            margin: auto; /*Margen*/
+            padding: 5px; /*Borde interno*/
+            width: 100%; /*Tamaño*/
+        }
+
         #new_creator input[type="submit"] { /*Estilo para el botón confirmar*/
             background-color: rgba(30, 55, 114, 0.550);
             box-shadow: 1 1 10px rgba(0, 0, 0); /*Sombra*/
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            margin: 15px 0px 0px 0px;
+            color: rgb(255, 255, 255); /*Colorde texto*/
+            padding: 10px 20px; /*margen interno vertical y horizontal*/
+            border: 2px groove rgb(115, 135, 214); /*Borde*/
+            border-radius: 8px; /*Radio de las esquinas*/
+            cursor: pointer; /*Estilo de puntero*/
+            margin: 15px 0px 0px 0px; /*margen superior, derecho, inferior, izquierdo*/
         }
 
-        #new_creator input[type="submit"]:hover {
-            background-color: rgb(20, 50, 134);
+        #new_creator input[type="submit"]:hover { /*Estilo para el botón al posar el mouse encima*/
+            background-color: rgb(20, 50, 134); /*Color de fonrdo*/
         }
 
-    </style>
+    </style> <!--Fin de estilos-->
 
     <title>Unirse a SHAIR</title> <!--ITítulo de la página-->
 
-</head>
+</head> <!--Fin de la cabeza-->
 
-<body>
+<body> <!--Inicio del cuerpo del proyecto-->
 
-    <div id="new_creator">
+    <div id="new_creator"> <!--Contenedor de registro-->
         
-        <h1>¡Únete a SHAIR!</h1>
+        <h1>¡Únete a SHAIR!</h1> <!--Título del contenedor-->
 
-        <form method="post">
-            <label for="nombre">Nombre:</label>
-            <input type="text" name="nombre" placeholder="Ingresa tu nombre de usuario" required>
+        <form method="post"> <!--Inicio formulario-->
+            <label for="nombre">Nombre:</label> <!--Campo de formulario-->
+            <input type="text" name="nombre" placeholder="Ingresa tu nombre de usuario" required> <!--Espacio para ingresar datos tipo texto, tiene nombre "nombre" y texto de explicación, es obligatorio-->
             
-            <label for="descripcion">Descripción:</label>
-            <input type="text" name="descripcion" placeholder="Ingresa una descripción" required></input>
+            <label for="descripcion">Descripción:</label> <!--Campo de formulario-->
+            <input type="text" name="descripcion" placeholder="Ingresa una descripción" required></input> <!--Espacio para ingresar datos tipo texto, tiene nombre "descripción" y texto de explicación, es obligatorio-->
 
-            <label for="codigo">Código:</label>
-            <input type="text" name="codigo" placeholder="Ingresa tu código de seguridad" required>
+            <label for="codigo">Código:</label> <!--Campo de formulario-->
+            <input type="password" name="codigo" placeholder="Ingresa tu código de seguridad" required> <!--Espacio para ingresar datos tipo password, tiene nombre "codigo" y texto de explicación, es obligatorio-->
 
-            <label for="correo">Correo:</label>
-            <input type="email" id="data_creator" name="correo" placeholder="Ingresa tu correo" required>
+            <label for="correo">Correo:</label> <!--Campo de formulario-->
+            <input type="email" id="data_creator" name="correo" placeholder="Ingresa tu correo" required> <!--Espacio para ingresar datos tipo email, tiene nombre "correo" y texto de explicación, es obligatorio-->
 
-            <label for="apoyo">Apoyo:</label>
-            <input type="text" name="apoyo" placeholder="Ingresa tu medio de apoyo" required>
+            <label for="apoyo">Apoyo:</label> <!--Campo de formulario-->
+            <input type="text" name="apoyo" placeholder="Ingresa tu medio de apoyo" required> <!--Espacio para ingresar datos tipo texto, tiene nombre "apoyo" y texto de explicación, es obligatorio-->
 
-            <input type="submit" name="crear" value="¡Unirte!">
+            <input type="submit" name="crear" value="¡Unirte!">  <!--Botón para enviar formulario con nombre "crear" y muestra en el botón "¡Unirte!"-->
             
-        </form>
+        </form> <!--Fin formulario-->
 
-        <?php
-        // Conectar a la base de datos 
-        $conexion = new mysqli('localhost', 'root', '', 'shair_v1.1.2_test');
-
-        if ($conexion->connect_error) {
-            die('Error en la conexión: ' . $conexion->connect_error);
-        }
-
-        // Operación de Creación
-        if (isset($_POST['crear'])) {
-            $nombre = $_POST['nombre'];
-            $descripcion = $_POST['descripcion'];
-            $codigo = $_POST['codigo'];
-            $correo = $_POST['correo'];
-            $apoyo = $_POST['apoyo'];
-
-            $sql = "INSERT INTO creador (nombre, descripcion, codigo, correo, apoyo) VALUES ('$nombre', '$descripcion', $codigo, '$correo', '$apoyo')";
-            if ($conexion->query($sql) === TRUE) {
-                echo '<p></p>';
-            } else {
-                echo 'Error al registrar: ' . $conexion->error;
-            }
-        }
-
-        // Cerrar la conexión
-        $conexion->close();
-        ?>
+        <?php //Código PHP
         
-    </div>
+        $conexion = new mysqli('localhost', 'root', '', 'shair_v1.1.2_test'); // Conectar a la base de datos (ubicación, usuario, contraseña, base de datos)
 
-</body>
+        if ($conexion->connect_error) { //Si condicional que verifica si existe la variable
+            die('Error en la conexión: ' . $conexion->connect_error); //Finalizar script y mostrar este mensaje
+        } //Fin si
 
-</html>
+        // Si ese Sí no se cumple, se ignora
+
+        if (isset($_POST['crear'])) { //Si condicional que verifica con isset que existe la variable $_POST con el nombre crear, esto verifica si se envió un formulario o se accionó el botón con ese nombre
+            $nombre = $_POST['nombre']; //Crea una variable con el valor enviado a un input con este nombre
+            $descripcion = $_POST['descripcion']; //Crea una variable con el valor enviado a un input con este nombre
+            $codigo = $_POST['codigo']; //Crea una variable con el valor enviado a un input con este nombre
+            $correo = $_POST['correo']; //Crea una variable con el valor enviado a un input con este nombre
+            $apoyo = $_POST['apoyo']; //Crea una variable con el valor enviado a un input con este nombre
+
+            $sql = "INSERT INTO creador (nombre, descripcion, codigo, correo, apoyo) VALUES ('$nombre', '$descripcion', $codigo, '$correo', '$apoyo')"; //Crea una consulta SQL que trata de insertar los valores de las variables ya declaradas en PHP
+            if ($conexion->query($sql) === TRUE) { //Si la inyección de datos es satisfactoria
+                echo '<p>¡Bienvenido a SHAIR!</p>'; //Mostrar este mensaje
+            } else { //Si no
+                echo 'Error al registrar: ' . $conexion->error; //Mostrar este mensaje
+            } //Fin si la inyección de datos es satisfactoria
+        } //Fin si existe la variable $_POST
+
+        $conexion->close(); // Cerrar la conexión
+        ?> <!--Fin código PHP-->
+        
+    </div> <!--Fin div de registro-->
+
+</body> <!--Fin cuerpo de del proyecto-->
+
+</html> <!--Fin código HTML-->
