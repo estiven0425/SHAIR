dd91e8116a5f5c678e8756da47e6f212dd5d8313
Estiven Montoya Torres <114525528+estiven0425@users.noreply.github.com>
2023-09-07 17:15:58 -0500

    Vistas

---
+++ code_shair/action_list_la.js
@@ -0,0 +1,5 @@
+const buttom_go_la = document.getElementById('list_la'); //llamamos etiqueta en una variable
+
+buttom_go_la.addEventListener('click', function() { //Variable tiene un evento click con la función
+  window.location.href = 'index_la.html'; //Buscar en la carpeta
+});
--- code_shair/action_menu.js
+++ code_shair/action_menu.js
@@ -1,14 +1,17 @@
 //Inicio script para mostrar/ocultar el menú lateral
 const action_show = document.getElementById('show_list'); //Creamos variables cuyo valor es una etiqueta HTML (Botón)
 const element_show = document.getElementById('lateral_menu'); //Creamos variables cuyo valor es una etiqueta HTML (Menú lateral)
-const menu = document.getElementById('menu_list');
-const forum = document.getElementById('forum');
+const move_list = document.getElementById('content'); //Llamamos la sección principal
 
 action_show.addEventListener('click', function() { //Variable cuando tenga un evento que es el click, realizará lo siguiente, una función
     if (element_show.style.maxWidth === '0px') { //Si la variable tiene un valor style - maxwidth = 0px ó style - display igual o equivalente a nada cambia lo siguiente
         element_show.style.maxWidth = '10%'; //Su style - maxwidth cambia a 10%
+        move_list.style.padding = '80px 5px 5px 10.3%'; //Cambiamos el padding
+        action_show.style.marginLeft = '70px'; //Recupera margen el botón
     } //Fin cambios
     else { //Si no
         element_show.style.maxWidth = '0px'; //Cambiar la propiedad a 0px
+        move_list.style.padding = '80px 5px 5px 5px'; //Restauramos el padding
+        action_show.style.marginLeft = '5px'; //pierde margen el botón
     } //Fin si no
 }); //Fin evento
