5e09c9e0e63d11286294da4eeb1a6788dbb1e45e
Estiven Montoya Torres <114525528+estiven0425@users.noreply.github.com>
2023-09-11 17:13:01 -0500

    Update new_product.php

--- code_shair_v1.0.0/new_product.php
+++ code_shair_v1.0.0/new_product.php
@@ -20,14 +20,15 @@
             padding: 10px; /*Quitamos padding por defecto*/
         }
 
-        #new_creator { /*Estilo al bloque de subida*/
+        #new_product { /*Estilo al bloque de subida*/
             align-items: center; /*Alineamos en el centro vertical*/
             background: radial-gradient(circle, rgb(190, 200, 244) 0%, rgba(40, 65, 224) 100%); /*Color de fondo*/
             border:  solid 2px rgb(135, 155, 224); /*Estilo, grosor y color de borde*/
             border-radius: 25px; /*Radio de las esquinas*/
             box-shadow: 0 0 20px rgba(30, 55, 214); /*Sombra*/
             display: flex; /*Display flexible*/
-            flex-wrap: wrap; /*Permitimos que se repitan filas*/
+            flex-direction: column; /*Dirección vertical*/
+            flex-wrap: nowrap; /*Permitimos que se repitan filas*/
             height: 400px; /*Altura*/
             justify-content: center; /*Alineamos en el centro horizontal*/
             margin: 25px; /*Margen*/
@@ -35,14 +36,14 @@
             width: 400px; /*Ancho*/
         }
 
-        #new_creator h1 { /*Estilo del título*/
+        #new_product h1 { /*Estilo del título*/
             color: rgb(228, 224, 20); /*Color de texto*/
             margin: auto;
             text-align: center; /*Alineamo el texto al centro*/
             text-shadow: 1px 1px 2px rgb(0, 0, 0); /*Sombra de texto*/
         }
 
-        #new_creator form { /*Estilo del formulario*/
+        #new_product form { /*Estilo del formulario*/
             align-items: center; /*Alineamos verticalmente en el centro*/
             display: flex; /*Pantalla flexible*/
             flex-direction: column; /*Ordenamos verticalmente*/
@@ -51,7 +52,7 @@
             text-align: center; /*Alineamos el texto al centro*/
         }
 
-        #new_creator input[type="text"] { /*Estilo para los input tipo texto*/
+        #new_product input[type="text"] { /*Estilo para los input tipo texto*/
             border: 1px groove rgb(135, 155, 224); /*Borde*/
             border-radius: 10px; /*Radio de las esquinas*/
             box-shadow: 0 0 20px rgba(30, 55, 114, 0.550); /*Sombra*/
@@ -60,25 +61,16 @@
             width: 100%; /*Tamaño*/
         }
 
-        #new_creator input[type="email"] { /*Estilo para los input tipo email*/
+        #new_product .menu { /*Estilo para los input tipo email*/
             border: 1px groove rgb(135, 155, 224); /*Borde*/
             border-radius: 10px; /*Radio de las esquinas*/
             box-shadow: 0 0 20px rgba(30, 55, 114, 0.550); /*Sombra*/
             margin: auto; /*Margen*/
             padding: 5px; /*Borde interno*/
-            width: 100%; /*Tamaño*/
+            width: 108%; /*Tamaño*/
         }
 
-        #new_creator input[type="password"] { /*Estilo para los input tipo email*/
-            border: 1px groove rgb(135, 155, 224); /*Borde*/
-            border-radius: 10px; /*Radio de las esquinas*/
-            box-shadow: 0 0 20px rgba(30, 55, 114, 0.550); /*Sombra*/
-            margin: auto; /*Margen*/
-            padding: 5px; /*Borde interno*/
-            width: 100%; /*Tamaño*/
-        }
-
-        #new_creator input[type="submit"] { /*Estilo para el botón confirmar*/
+        #new_product input[type="submit"] { /*Estilo para el botón confirmar*/
             background-color: rgba(30, 55, 114, 0.550);
             box-shadow: 1 1 10px rgba(0, 0, 0); /*Sombra*/
             color: rgb(255, 255, 255); /*Colorde texto*/
@@ -89,34 +81,43 @@
             margin: 15px 0px 0px 0px; /*margen superior, derecho, inferior, izquierdo*/
         }
 
-        #new_creator input[type="submit"]:hover { /*Estilo para el botón al posar el mouse encima*/
+        #new_product input[type="submit"]:hover { /*Estilo para el botón al posar el mouse encima*/
             background-color: rgb(20, 50, 134); /*Color de fonrdo*/
         }
 
     </style> <!--Fin de estilos-->
 
-    <title>Crear un espacio en SHAIR</title> <!--ITítulo de la página-->
+    <title>Crea tu producto a SHAIR</title> <!--ITítulo de la página-->
 
 </head> <!--Fin de la cabeza-->
 
 <body> <!--Inicio del cuerpo del proyecto-->
 
-    <div id="new_creator"> <!--Contenedor de registro-->
+    <div id="new_product"> <!--Contenedor de registro-->
         
-        <h1>¡Crea tu espacio!</h1> <!--Título del contenedor-->
+        <h1>¡Crea tu producto!</h1> <!--Título del contenedor-->
 
         <form method="post">
             <label for="nombre">Nombre del Producto:</label>
             <input type="text" name="nombre" placeholder="Ingresa el nombre del producto" required>
+
+            <label for="descripcion">Descripción del Producto:</label>
+            <input type="text" name="descripcion" placeholder="Ingresa la descripción del producto" required>
             
             <label for="categoria">Categoría:</label>
-            <select name="categoria" required>
+            <select class="menu" name="categoria" required>
+                <option value="aplicación">Aplicación</option>
                 <option value="videojuego">Videojuego</option>
                 <option value="web">Web</option>
             </select>
 
             <label for="plataforma">Plataforma:</label>
-            <input type="text" name="plataforma" placeholder="Ingresa la plataforma" required>
+            <select class="menu" name="plataforma" required>
+                <option value="Android">Android</option>
+                <option value="Linux">Linux</option>
+                <option value="MacOs">MacOs</option>
+                <option value="Windows">Windows</option>
+            </select>
 
             <input type="submit" name="crear_producto" value="Crear Producto">
         </form>
@@ -133,11 +134,11 @@
             $nombre_producto = $_POST['nombre'];
             $categoria = $_POST['categoria'];
             $plataforma = $_POST['plataforma'];
-
-            $sql = "INSERT INTO producto (nombre, categoria, plataforma) VALUES ('$nombre_producto', '$categoria', '$plataforma')";
+            $descripcion = $_POST['descripcion'];
+            $sql = "INSERT INTO producto (nombre, descripcion, categoria, plataforma) VALUES ('$nombre_producto', '$descripcion', '$categoria', '$plataforma')";
             
             if ($conexion->query($sql) === TRUE) {
-                echo '<p>Espacio creado exitosamente</p>';
+                echo '<p>producto creado exitosamente</p>';
             } else {
                 echo 'Error al crear el producto: ' . $conexion->error;
             }
