40d7a5d2a930eab047b6e0e56cd478c0eba45a1b
Estiven Montoya Torres <114525528+estiven0425@users.noreply.github.com>
2023-09-14 17:11:27 -0500

    Add files via upload

---
+++ code_shair_v1.0.0/action_session_open.js
@@ -0,0 +1,5 @@
+const buttom_session = document.getElementById('login_alt'); //llamamos etiqueta en una variable
+
+buttom_session.addEventListener('click', function() { //Variable tiene un evento click con la función
+  window.location.href = 'http://localhost/SHAIR-main/code_shair_v1.0.0/session_open.php'; //Buscar en la carpeta
+});
