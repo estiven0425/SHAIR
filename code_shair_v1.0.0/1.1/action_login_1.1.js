b6c8ae73190eefb428a922812f521b103c920a36
estiven0425 <estivenmoto6@gmail.com>
2023-10-12 00:08:17 -0500

    Cambio nombre carpeta

--- code_shair_v1.0.0/1.1/action_login_1.1.js
+++
@@ -1,5 +0,0 @@
-const buttom_login = document.getElementById('login'); //llamamos etiqueta en una variable
-
-buttom_login.addEventListener('click', function() { //Variable tiene un evento click con la función
-  window.location.href = './new_creator.php'; //Buscar en la carpeta
-});
--- code_shair_v1.0.0/1.1/action_session_open_1.1.js
+++
@@ -1,5 +0,0 @@
-const buttom_session = document.getElementById('login_alt'); //llamamos etiqueta en una variable
-
-buttom_session.addEventListener('click', function() { //Variable tiene un evento click con la función
-  window.location.href = './session_open.php'; //Buscar en la carpeta
-});
